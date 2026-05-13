3c98f7dd3360f8847dda4299f9899279cd3c8495
Marco Aurélio <marcauremaza@gmail.com>
2026-05-13 20:44:53 -0300

    First Commit

---
+++ src/main/java/gerenciamento/biblioteca/api/auth2/Application.java
@@ -0,0 +1,13 @@
+package gerenciamento.biblioteca.api.auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciamento/biblioteca/api/auth2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamento.biblioteca.api.auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
